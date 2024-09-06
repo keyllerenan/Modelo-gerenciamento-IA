5533cad9f5afa7c85c5a24d1c57ab724695031f1
keylle <keyllerenan@gmail.com>
2024-09-05 23:31:21 -0300

    feat: test2

---
+++ assets/app/dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        nome: "Luvas Esportivas (★) | Caixa de Pandora",
+        exterior: "Nova de Fábrica",
+        float: 0.0015,
+        preco: 52.000,
+        link: "https://csgoempire.com/item/248708631"
+    }, 
+    {
+        nome: "Faca Butterfly ★ | Gamma Doppler",
+        exterior: "Nova de Fábrica",
+        float: 0.0012,
+        preco: 15.930,
+        link: "https://csgoempire.com/item/246141880"
+    },
+    {
+        nome: "StatTrak™ M4A4 | Howl",
+        exterior: "Nova de Fábrica",
+        float: 0.0020,
+        preco: 13.100,
+        link: "https://csgoempire.com/item/249901510"
+    }
+];
