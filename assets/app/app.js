b1832d491a3d7acacb72af6e803545cde9e17fee
keylle <keyllerenan@gmail.com>
2024-09-09 22:27:46 -0300

    feat: add

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,1 +1,39 @@
-console.log(dados[1].exterior);
+function pesquisar () {
+    let section = document.getElementById("resultados-pesquisa");
+//console.log(section);
+
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+//console.log(campoPesquisa)
+
+if (campoPesquisa == "") {
+    section.innerHTML = "Nenhum item foi encontrado"
+    return    
+}
+
+campoPesquisa = campoPesquisa.toLowerCase();
+
+let resultados = "";
+let nome = "";
+let descricao = "";
+
+for (let dado of dados) {
+    nome = dado.nome.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+     if (dado.nome.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa)){
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+            <a href="${dado.link}" target="_blank">${dado.nome}</a>
+            </h2>
+        <p class="descricao-meta">${dado.des}<a href="#" target="_blank">Mais informações</a>
+        </p>
+        </div>
+        `
+     }
+};
+
+//Atribui os resultados gerados à seção HTML
+section.innerHTML = resultados;
+}
+
+
--- assets/app/dados.js
+++ assets/app/dados.js
@@ -1,9 +1,10 @@
-let dados = [
+let dado = [
     {
         nome: "Luvas Esportivas (★) | Caixa de Pandora",
         exterior: "Nova de Fábrica",
         float: 0.0015,
         preco: 52.000,
+        descricao: "O tecido sintético fornece resistência e um visual único a estas luvas atléticas.Estas luvas preta e roxas têm um padrão sutil impresso nas palmas. Obrigatórias para qualquer especialista em demolição com mãos suadas.",
         link: "https://csgoempire.com/item/248708631"
     }, 
     {
@@ -11,6 +12,7 @@ let dados = [
         exterior: "Nova de Fábrica",
         float: 0.0012,
         preco: 15.930,
+        descricao: "Esta é uma faca butterfly, também conhecida como faca borboleta. A característica que mais define esta arma é a abertura que permite à lâmina entrar ou sair da empunhadura livremente num eixo giratório. Devido a isto, as facas butterfly são ilegais em muitos países. Esta arma em particular foi pintada com tintas pretas e prateadas metalizadas usando um meio marmorizante e depois revestida com um acabamento especial lustrado.",
         link: "https://csgoempire.com/item/246141880"
     },
     {
@@ -18,6 +20,7 @@ let dados = [
         exterior: "Nova de Fábrica",
         float: 0.0020,
         preco: 13.100,
+        descricao: "Mais preciso e menos poderoso que o AK-47, o seu correspondente, o M4A4 é o rifle de assalto automático dos CTs. Esta arma foi pintada com o desenho de um lobo feroz.",
         link: "https://csgoempire.com/item/249901510"
     }
 ];
